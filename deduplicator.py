ce1f8f0fd6b8360e45234341dee9c453de27a0af
Spiros Evangelatos <sevangelatos@gmail.com>
2018-02-27 01:50:29 +0200

    Major refactoring

--- deduplicator.py
+++ deduplicator.py
@@ -10,65 +10,64 @@ read_blocksize = 16*1024
 
 fsencode = lambda x:x
 fsdecode = lambda x:x
+stream = sys.stdin
 
+# Python 3 compatibility
 if sys.version_info[0] >= 3:
     fsencode = os.fsencode
     fsdecode = os.fsdecode
+    stream = sys.stdin.buffer
 
-class file_info(object):
+class FileInfo(object):
+    "Filename and stat information "
     def __init__(self, f):
         self.filename = f
         self.stat = os.stat(f)
-
-    def size(self):
-        return self.stat.st_size
 
     def regular_file(self):
         mode = self.stat[stat.ST_MODE]
         return stat.S_ISREG(mode) and not stat.S_ISLNK(mode)
 
+    def strname(self):
+        return fsdecode(self.filename)
+
 def sha1(filename):
-    f = open(filename, 'rb')
+    "Comprehensive SHA1 hash of entire file"
     h = hashlib.sha1()
-    block = f.read(read_blocksize)
-    while block:
-        h.update(block)
+    with open(filename, 'rb') as f:
         block = f.read(read_blocksize)
+        while block:
+            h.update(block)
+            block = f.read(read_blocksize)
     return h.digest()
 
 def fast_hash(filename):
-    f = open(filename, 'rb')
+    "Quick MD5 hash of first block only"
     h = hashlib.md5()
-    block = f.read(read_blocksize)
-    h.update(block)
+    with open(filename, 'rb') as f:
+        block = f.read(read_blocksize)
+        h.update(block)
     return h.digest()
 
-def group_by_size(filenames):
-    by_size = {}
+def stat_files(filenames):
+    "Produce FileInfo for all non-empty files"
     for fname in filenames:
         try:
-            info = file_info(fname)
+            info = FileInfo(fname)
         except OSError:
             sys.stderr.write("Could not stat " + fsdecode(fname) + " skipping...\n")
             continue
-        if not info.regular_file():
+        if not info.regular_file() or info.stat.st_size == 0:
             continue
-        size = info.size()
-        if size in by_size:
-            by_size[size].append(info)
-        else:
-            by_size[size] = [info]
-    return [item for item in by_size.values() if len(item) > 1 ]
+        yield info
 
-def group_by_inode(files):
-    by_inode = {}
+def group_by(files, key):
+    "Group files by key function"
+    by_group = {}
     for f in files:
-        inode = f.stat.st_ino
-        if inode in by_inode:
-            by_inode[inode].append(f)
-        else:
-            by_inode[inode] = [f]
-    return by_inode
+        by_group.setdefault(key(f), []).append(f)
+    return by_group
+
     
 def group_by_hash(files, hash_function):
     by_inode = group_by_inode(files)
@@ -81,7 +80,7 @@ def group_by_hash(files, hash_function):
             h = hash_function(by_inode[inode][0].filename)
         except IOError:
             sys.stderr.write("Could not get hash for " +
-                             by_inode[inode][0].filename + " skipping...\n")
+                             by_inode[inode][0].strname() + " skipping...\n")
             continue
         if h in by_hash:
             by_hash[h] = by_hash[h] + by_inode[inode]
@@ -90,59 +89,43 @@ def group_by_hash(files, hash_function):
     
     return [files for files in by_hash.values() if len(files) > 1]
 
-def group_identical(files):
-    return group_by_hash(files, sha1)
-
-def listing_print(l):
+def listing_print(files):
     print('')
-    for i in l:
-        print("{} {} {}".format(i.stat.st_nlink, i.stat.st_ino, fsdecode(i.filename)))
+    for f in files:
+        print("{} {} {}".format(f.stat.st_nlink, f.stat.st_ino, f.strname()) )
     print('')
 
 def hardlink(src, dest):
     if src.stat.st_dev != dest.stat.st_dev:
         print("{} and {} are not on the same device"\
-            .format(fsdecode(src.filename), fsdecode(dest.filename)), file=sys.stderr)
+            .format(src.strname(), dest.strname()), file=sys.stderr)
         return False
 
     try:
         backup = dest.filename + fsencode(".bak")
         os.rename(dest.filename, backup)
-        print("linking...\n{} to \n{}".format(fsdecode(src.filename), fsdecode(dest.filename)))
+        print("linking...\n{} to \n{}".format(src.strname(), dest.strname()))
         os.link(src.filename, dest.filename)
         os.unlink(backup)
     except Exception as ex:
         print(ex, file=sys.stderr)
         return False
     return True
-
+    
 def make_hardlinks(files):
-    # group by inode
-    by_inode = {}
-    best_inode = None
-    nlink_max = 0
-    for f in files:
-        inode = f.stat.st_ino
-        if f.stat.st_nlink >= nlink_max:
-            nlink_max = f.stat.st_nlink
-            best_inode = inode
-        if inode in by_inode:
-            by_inode[inode].append(f)
-        else:
-            by_inode[inode] = [f]
+    by_inode = group_by_inode(files)
     # If there are no files or all files are hardlinked with eachother...
     if len(by_inode) <= 1:
         return
 
-    master = by_inode[best_inode][0]
+    master = max(files, key=lambda f: f.stat.st_nlink)
     for inode in by_inode:
-        if inode == best_inode:
+        if inode == master.stat.st_ino:
             continue
         for f in by_inode[inode]:
             if not hardlink(master, f):
                 print("Could not hardlink {} to {} ...skipping"\
-                      .format(fsdecode(master.filename), fsdecode(f.filename)), file=sys.stderr)
-
+                      .format(master.strname(), f.strname()), file=sys.stderr)
 
 def system(cmd, params):
     command = [fsencode(i) for i in cmd.split()]
@@ -152,40 +135,71 @@ def system(cmd, params):
         raise ChildProcessError("Could not execute: " + fsdecode(ex))
     return status
 
-def print_help():
-    print('Usage: dupes [--exec command | --hardlink]', file=sys.stderr)
+def group_by_size(files):
+    return group_by(files, lambda f: f.stat.st_size).values()
+
+def group_by_dev(files):
+    return group_by(files, lambda f: f.stat.st_dev).values()
+
+def group_by_inode(files):
+    return group_by(files, lambda f: f.stat.st_ino)
     
-def filenames_stdin():
-    stream = sys.stdin
-    if sys.version_info[0] >= 3:
-        stream = sys.stdin.buffer
-    for fname in stream.readlines():
-        yield fname[:-1] #discard newline
+def filter_solo(items_list):
+    return (i for i in items_list if len(i) > 1)
 
-#parse parameters
-try:
-    optlist, args = getopt.getopt(sys.argv[1:], '', ['exec=', 'hardlink'])
-    if args:
-        raise RuntimeError(str(args))
-except:
-    print_help()
-    sys.exit(1)
+def print_help():
+    print('Usage: dupes [--exec command | --hardlink] [-0]', file=sys.stderr)
+    
+def read_filenames(stream, separator = b"\n"):
+        buff = stream.read(read_blocksize)
+        remain = b""
+        while buff:
+            tokens = (remain + buff).split(separator)
+            remain = b""
+            # Keep remainder bytes if needed...
+            if len(tokens) and len(tokens[-1]):
+                tokens, remain = tokens[:-1], tokens[-1]
 
-#setup action on duplicates
-action = listing_print
-if optlist and optlist[0][0] == '--hardlink':
-    action = make_hardlinks 
-elif optlist:
-    cmd = optlist[0][1]
-    action = lambda params: system(cmd, params)
+            for tok in tokens:
+                if tok: yield tok
+            buff = stream.read(read_blocksize)
+        if remain:
+            yield remain
 
+class Options(object):
+    "Holds script options"
+    def parse(self):
+        "Initialize options from command line"
+        try:
+            optlist, args = getopt.getopt(sys.argv[1:], '0', ['exec=', 'hardlink', 'dry-run'])
+            if args:
+                raise RuntimeError(str(args))
+        except:
+            print_help()
+            sys.exit(1)
+        
+        self.action = listing_print # Sction on duplicates
+        self.separator = b'\n'      # Files separator
+        self.dry_run = False        # Dry run only
+        
+        for opt in optlist:
+            if opt[0] == '--hardlink':
+                self.action = make_hardlinks
+            if opt[0] == "--exec":
+                self.action = lambda params: system(opt[1], params)
+            if opt[0] == "--dry-run":
+                self.dry_run = True
+            if opt[0] == "-0":
+                self.separator = b'\0'
 
-for same_size in group_by_size( filenames_stdin() ):
-    for same_fhash in group_by_hash(same_size, fast_hash):
-        for identical in group_identical(same_fhash):
-            # Don't do anything for lonely files
-            if len(identical) <= 1:
-                continue
+def main():
+    opts = Options()
+    opts.parse()
+    
+    for same_size in filter_solo( group_by_size( stat_files(read_filenames(stream, opts.separator)) )):
+        for same_fhash in filter_solo( group_by_hash(same_size, fast_hash)):
+            for identical in filter_solo( group_by_hash(same_fhash, sha1)):
+                opts.action(identical)
 
-            action(identical)
-
+if __name__ == "__main__":
+    main()
